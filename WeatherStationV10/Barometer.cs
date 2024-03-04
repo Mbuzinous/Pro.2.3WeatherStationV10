7e8b6c2e45c5022df4f8fcb80589ebe05297a2dc
Kevin Waweru <kew001@edu.zealand.dk>
2024-03-04 14:57:50 +0100

    Add project files.

---
+++ WeatherStationV10/Barometer.cs
@@ -0,0 +1,64 @@
+﻿namespace WeatherStationV10
+{
+    /// <summary>
+    ///  Simple barometer, measuring pressure in hPa (hectopascal)
+    /// </summary>
+    public class Barometer
+    {
+        #region Instance fields
+        private double _pressureInHPa;
+        private string _stormy = "The weather is Stormy";
+        private string _rainy = "The weather is Rainy";
+        private string _changing = "The weather is Changing";
+        private string _fair = "The weather is Fair";
+        private string _veryDry = "The weather is Very dry";
+        private string _nice = "All weather is nice!";
+        #endregion
+
+        #region Constructor
+        public Barometer()
+        {
+            _pressureInHPa = 1000.0;
+        }
+        #endregion
+
+        #region Properties
+        public double Pressure
+        {
+            get { return _pressureInHPa; }
+            set { _pressureInHPa = value; }
+        }
+
+        public string WeatherDescription
+        {
+            get 
+            { 
+                if (Pressure >= 1040)
+                {
+                    return _veryDry;
+                }
+                else if (Pressure >= 1020)
+                {
+                    return _fair;
+                }
+                else if (Pressure >= 1000)
+                {
+                    return _changing;
+                }
+                else if (Pressure >= 980)
+                {
+                    return _rainy;
+                }
+                else if (Pressure < 980)
+                {
+                    return _stormy;
+                }
+                else
+                {
+                    return "All weather is nice!";
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ WeatherStationV10/InsertCodeHere.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WeatherStationV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            Barometer barometer = new Barometer();
+
+            Console.WriteLine(barometer.Pressure);
+            Console.WriteLine(barometer.WeatherDescription);
+
+            barometer.Pressure = 979;
+            Console.WriteLine(barometer.Pressure);
+            Console.WriteLine(barometer.WeatherDescription);
+
+            barometer.Pressure = 980;
+            Console.WriteLine(barometer.Pressure);
+            Console.WriteLine(barometer.WeatherDescription);
+
+            barometer.Pressure = 1000;
+            Console.WriteLine(barometer.Pressure);
+            Console.WriteLine(barometer.WeatherDescription);
+
+            barometer.Pressure = 1020;
+            Console.WriteLine(barometer.Pressure);
+            Console.WriteLine(barometer.WeatherDescription);
+
+            barometer.Pressure = 1040;
+            Console.WriteLine(barometer.Pressure);
+            Console.WriteLine(barometer.WeatherDescription);
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
